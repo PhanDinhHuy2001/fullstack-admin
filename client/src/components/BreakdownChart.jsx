008fe84fac6ee0849f0af32598d091babd00177f
Phan Dinh Huy <huydinhphan2001@gmail.com>
2023-03-05 12:06:20 +0700

    init commit

---
+++ client/src/components/BreakdownChart.jsx
@@ -0,0 +1,104 @@
+import React from "react";
+import { Box, Typography, useTheme } from "@mui/material";
+import { useGetSalesQuery } from "../state/api";
+import { ResponsivePie } from "@nivo/pie";
+
+
+const BreakdownChart = ({ isDashboard = false }) => {
+  const { data, isLoading } = useGetSalesQuery();
+  const theme = useTheme();
+
+  if (!data || isLoading) return "Loading...";
+
+  const colors = [
+    theme.palette.secondary[500],
+    theme.palette.secondary[300],
+    theme.palette.secondary[300],
+    theme.palette.secondary[500],
+  ];
+  const formattedData = Object.entries(data.salesByCategory).map(
+    ([category, sales], i) => ({
+      id: category,
+      label: category,
+      value: sales,
+      color: colors[i],
+    })
+  );
+
+  return (
+    <Box
+      height={isDashboard ? "400px" : "100%"}
+      width={undefined}
+      minHeight={isDashboard ? "325px" : undefined}
+      minWidth={isDashboard ? "325px" : undefined}
+      position="relative"
+    >
+      <ResponsivePie
+        data={formattedData}
+        margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+        innerRadius={0.5}
+        padAngle={0.7}
+        cornerRadius={3}
+        activeOuterRadiusOffset={8}
+        borderWidth={1}
+        borderColor={{
+          from: "color",
+          modifiers: [["darker", 0.2]],
+        }}
+        arcLinkLabelsSkipAngle={10}
+        arcLinkLabelsTextColor="#333333"
+        arcLinkLabelsThickness={2}
+        arcLinkLabelsColor={{ from: "color" }}
+        arcLabelsSkipAngle={10}
+        arcLabelsTextColor={{
+          from: "color",
+          modifiers: [["darker", 2]],
+        }}
+        legends={[
+          {
+            anchor: "bottom",
+            direction: "row",
+            justify: false,
+            translateX: 0,
+            translateY: 56,
+            itemsSpacing: 0,
+            itemWidth: 100,
+            itemHeight: 18,
+            itemTextColor: "#999",
+            itemDirection: "left-to-right",
+            itemOpacity: 1,
+            symbolSize: 18,
+            symbolShape: "circle",
+            effects: [
+              {
+                on: "hover",
+                style: {
+                  itemTextColor: "#000",
+                },
+              },
+            ],
+          },
+        ]}
+      />
+      <Box
+        position="absolute"
+        top="50%"
+        left="50%"
+        color={theme.palette.secondary[400]}
+        textAlign="center"
+        pointerEvents="none"
+        sx={{
+          transform: isDashboard
+            ? "translate(-75%, -170%)"
+            : "translate(-50%, -100%)",
+        }}
+      >
+        <Typography variant="h6">
+          {!isDashboard && "Total:"} ${data.yearlySalesTotal}
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default BreakdownChart;
